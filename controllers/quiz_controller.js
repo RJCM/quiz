9c80acd69dc80bc1e7e26efbf3dd932fff4f23a6
Rafael Curiel <curiel.rafael@gmail.com>
2015-07-27 21:11:28 -0430

    paso 9: lista de preguntas c

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,14 +3,16 @@ var models = require('../models/models.js');
 // GET /quizes
 exports.index = function(req, res){
   models.Quiz.findAll().then(function(quizes) { 
-    res.render('quizes/index.ejs',{ quizes: quizes})
+    res.render('quizes/index.ejs',{ quizes: quizes});
+  })  
 };
 
 
 // GET /quizes/:id
 exports.show = function(req, res){
   models.Quiz.find(req.params.quizId).then(function(quiz) { 
-    res.render('quizes/show',{ quiz: quiz})
+    res.render('quizes/show',{ quiz: quiz});
+  })
 };
 
 // GET /quizes/:Id/answer
