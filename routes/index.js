60709541f061eab1d2b0f87a03bdac33005e2a9d
Rafael Curiel <curiel.rafael@gmail.com>
2015-07-27 22:56:20 -0430

    paso 10: autoload

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,17 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz ..!' });
+// Página de entrada (home page)
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz' });
 });
 
-router.get('/author', function(req, res, next) {
-  res.render('author', { author: 'Rafael Jose Curiel Medina', photo: 'rjcm.jpg' });
-});
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
 
-// Definicion e Rutas de /quiz
-router.get('/quizes', 			   quizController.index);
-router.get('/quizes/:quizId(\\d+)', 	   quizController.show);
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
