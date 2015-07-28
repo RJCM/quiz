4879cb189a74358cb9b29f261f9cc705dd62eec3
Rafael Curiel <curiel.rafael@gmail.com>
2015-07-27 21:39:19 -0430

    paso 9: lista de preguntas d

--- models/models.js
+++ models/models.js
@@ -38,10 +38,8 @@ exports.Quiz = Quiz;
 sequelize.sync().then(function() {
   Quiz.count().then(function(count){
     if (count===0) {
-       Quiz.create({pregunta: 'Capital de Italia',
-		    respuesta: 'Roma'});
-       Quiz.create({pregunta: 'Capital de Portugal',
-		    respuesta: 'Lisboa'})
+       Quiz.create({pregunta: 'Capital de Italia',   respuesta: 'Roma'});
+       Quiz.create({pregunta: 'Capital de Portugal', respuesta: 'Lisboa'})
        .then(function(){console.log('DB Inicializada..!')});
     };
   });
