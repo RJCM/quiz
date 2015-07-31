57c8823129b63202a7d879977c4f8a274e63fcf2
Rafael Curiel <curiel.rafael@gmail.com>
2015-07-31 17:48:11 -0430

    Ejercicio P2P, Modulo 8

--- models/models.js
+++ models/models.js
@@ -34,9 +34,9 @@ exports.Quiz = Quiz;
 sequelize.sync().then(function() {
   Quiz.count().then(function(count){
     if (count === 0) {
-	  Quiz.create({ pregunta:'Capital de Italia',respuesta:'Roma'});
-	  Quiz.create({ pregunta:'Capital de Francia',respuesta:'Paris'});
-	  Quiz.create({ pregunta:'Capital de Portugal',respuesta:'Lisboa'}
+	  Quiz.create({ pregunta:'Capital de Italia',respuesta:'Roma',tema: 'ocio'});
+	  Quiz.create({ pregunta:'Capital de Francia',respuesta:'Paris',tema: 'ocio'});
+	  Quiz.create({ pregunta:'Capital de Portugal',respuesta:'Lisboa',tema: 'ocio'}
 	  ).then(function(){console.log('DB inicializada..!')});
      };
   });
--- models/quiz.js
+++ models/quiz.js
@@ -3,6 +3,7 @@
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('Quiz', 
     {pregunta:{type:DataTypes.STRING,validate:{notEmpty:{msg:"Error..! Falta Pregunta"}}},
-     respuesta:{type:DataTypes.STRING,validate:{notEmpty:{msg:"Error..! Falta Respuesta"}}}
+     respuesta:{type:DataTypes.STRING,validate:{notEmpty:{msg:"Error..! Falta Respuesta"}}},
+     tema:{type:DataTypes.STRING,validate:{notEmpty:{msg:"Error..! Falta Tema"}}}
     });
 }
