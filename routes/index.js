e5c3c1066da91d78c860f4f6fa5661e9005607e4
Rafael Curiel <curiel.rafael@gmail.com>
2015-08-18 20:27:48 -0430

    paso 16: autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var sessionController = require('../controllers/session_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
@@ -14,6 +15,11 @@ router.get('/author', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+
+//Definición de rutas de sesion
+router.get('/login', sessionController.new);//pantalla de login
+router.post('/login', sessionController.create);//procesado de login, crea sesion
+router.get('/logout', sessionController.destroy);//destruye sesion
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
