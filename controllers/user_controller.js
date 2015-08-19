5296c26528158986fe08560ad0adb4b8a2461ab3
Rafael Curiel <curiel.rafael@gmail.com>
2015-08-19 18:36:05 -0430

    paso 16: p2p

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -6,9 +6,9 @@ var users = { admin: {id:1, username:"admin", password:"1234"},
 // Si autenticación falla o hay errores se ejecuta callback(error).
 exports.autenticar = function(login, password, callback) {
     if(users[login]){
-        if(password === users[login].password){
+        if(users[login].password === password){
             callback(null, users[login]);
         }
-        else { callback(new Error('Password erróneo.')); }
+        else { callback(new Error('El password no es correcto !!!')); }
     } else { callback(new Error('No existe el usuario.'));}
 };
